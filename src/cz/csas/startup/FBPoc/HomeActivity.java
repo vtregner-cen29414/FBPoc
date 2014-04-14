bcdd3680a82780a6123cd7cbe87b09e3b77b149d
vtregner <vaclav.tregner@gmail.com>
2014-04-14 13:14:42 +0200

    repo init

---
+++ src/cz/csas/startup/FBPoc/HomeActivity.java
@@ -0,0 +1,104 @@
+package cz.csas.startup.FBPoc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import com.facebook.*;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+
+public class HomeActivity extends Activity {
+    private static final String TAG = "FBPoc";
+    UiLifecycleHelper uiHelper;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        LoginButton authButton = (LoginButton) findViewById(R.id.authButton);
+        TextView currentUser = (TextView) findViewById(R.id.currentUser);
+        currentUser.setVisibility(View.GONE);
+
+        uiHelper = new UiLifecycleHelper(this, new Session.StatusCallback() {
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                onSessionStateChange(session, state, exception);
+            }
+        });
+
+        uiHelper.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            Log.i(TAG, "Logged in...");
+
+            // Request user data and show the results
+            Request.newMeRequest(session, new Request.GraphUserCallback() {
+                @Override
+                public void onCompleted(GraphUser user, Response response) {
+                    if (user != null) {
+                        TextView currentUser = (TextView) findViewById(R.id.currentUser);
+                        currentUser.setVisibility(View.VISIBLE);
+                        currentUser.setText("Welcome " + user.getFirstName() + " " + user.getLastName());
+                    }
+                }
+            }).executeAsync();
+
+        } else if (state.isClosed()) {
+            Log.i(TAG, "Logged out...");
+            TextView currentUser = (TextView) findViewById(R.id.currentUser);
+            currentUser.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // For scenarios where the main activity is launched and user
+        // session is not null, the session state change notification
+        // may not be triggered. Trigger it if it's open/closed.
+        Session session = Session.getActiveSession();
+        if (session != null &&
+                (session.isOpened() || session.isClosed()) ) {
+            onSessionStateChange(session, session.getState(), null);
+        }
+
+        uiHelper.onResume();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+
+}
